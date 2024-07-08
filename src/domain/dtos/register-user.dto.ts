5f2afe52af50371501bc9eae1d1ebe4881663ba1
Alejoboga20 <alejoboga19@gmail.com>
2024-07-08 14:58:29 -0300

    register user dto

---
+++ src/domain/dtos/register-user.dto.ts
@@ -0,0 +1,22 @@
+import { regularExps } from '../../config';
+
+export class RegisterUserDto {
+	// Private constructor to only allow the static method to create instances
+	private constructor(
+		public readonly name: string,
+		public readonly email: string,
+		public readonly password: string
+	) {}
+
+	static create(object: { [key: string]: any }): [string?, RegisterUserDto?] {
+		const { name, email, password } = object;
+
+		if (!name) return ['Missing name'];
+		if (!email) return ['Missing email'];
+		if (!regularExps.email.test(email)) return ['Invalid email'];
+		if (!password) return ['Missing password'];
+		if (password.length < 6) return ['Password must be at least 6 characters long'];
+
+		return [undefined, new RegisterUserDto(name, email, password)];
+	}
+}
