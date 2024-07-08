58df6d21f83b630138af91681e939afef53acf4c
Alejoboga20 <alejoboga19@gmail.com>
2024-07-08 18:28:33 -0300

    use jwt seed

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,4 +5,5 @@ export const envs = {
 	MONGO_URL: get('MONGO_URL').required().asString(),
 	MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
 	PORT: get('PORT').required().asPortNumber(),
+	JWT_SEED: get('JWT').required().asString(),
 };
