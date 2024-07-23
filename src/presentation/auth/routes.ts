e16290f0054e15f44711e94168d0f8a4f7292658
Alejoboga20 <alejoboga19@gmail.com>
2024-07-23 15:55:38 -0300

    add email service

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { AuthController } from './controller';
-import { AuthService } from './auth.service';
+import { AuthService } from '../services/auth.service';
 
 export class AuthRoutes {
 	static get routes(): Router {
